0299df90c8160915ff821b53226b35e85d6a5888
vihoqiqi <921827439@qq.com>
2021-05-12 16:28:59 +0800

    commit four

--- student.java
+++ student.java
@@ -1,6 +1,7 @@
-import java.util.*;
-class student<string> {
+import java.util.*
+class student{
     private string name;
     private string address;
+    private string email;
+    private String telephone;   //新增电话属性
 }
-
